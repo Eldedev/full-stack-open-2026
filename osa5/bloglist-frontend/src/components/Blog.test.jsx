2beda8b57f51c5d34b08095878268c513993b1bb
Elde <44910837+Eldedev@users.noreply.github.com>
2026-06-08 18:19:47 +0300

    ex 5.14

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './blog'
 
 test('renders content', () => {
@@ -17,4 +18,29 @@ test('renders content', () => {
 
     const element = screen.getAllByText('Component testing is done with react-testing-library', { exact: false })
     expect(element).toBeDefined()
+})
+
+test('clicking the button calls event handler once and shows extra info', async () => {
+    const user1 = {
+        username: 'Elde',
+        name: 'Elias'
+    }
+    const blog = {
+        title: 'Component testing is done with react-testing-library',
+        author: 'Alex Stubb',
+        url: 'Herewego.eu',
+        user: user1
+    }
+
+    render(
+        <Blog blog={blog} />
+    )
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const div = document.querySelector('.blog')
+
+    expect(div).toBeVisible()
 })
